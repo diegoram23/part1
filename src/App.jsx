f3b31b5fb8ca8c213473701d7b44bf634e14f320
diegoram23 <diego.pta.oc@gmail.com>
2023-02-25 11:22:55 -0800

    completed exercise 1.12

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,33 @@
 import React from "react"
-import { useState } from "react"
-import Statistics from "./Statistics"
-import StatisticsLine from "./StatisticsLine"
-
-
-const Button = (props) => {
-  return (
-    <button onClick={props.handleClick} >{props.text}</button>
-  )
-}
-
+import { useState } from 'react'
 
 const App = () => {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [total, setTotal] = useState(0)
-  const positive = (total === 0 ? 0 : (good / total * 100).toFixed(2))
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
+  const [selected, setSelected] = useState(0)
 
-  const increaseGood = () => {
-    setGood(good + 1)
-    setTotal(total + 1)
-  }
+  const randomAnecdote = () => {
 
-  const increaseNeutral = () => {
-    setNeutral(neutral + 1)
-    setTotal(total + 1)
-  }
+    let randomNumber = Math.floor(Math.random() * anecdotes.length)
+    setSelected(randomNumber)
 
-  const increaseBad = () => {
-    setBad(bad + 1)
-    setTotal(total + 1)
   }
 
   return (
-
     <div>
-      <h2>give feedback</h2>
-      <Button handleClick={increaseGood} text='Good' />
-      <Button handleClick={increaseNeutral} text='Neutral' />
-      <Button handleClick={increaseBad} text='Bad' />
-
-      <Statistics 
-      good={good}
-      neutral={neutral}
-      bad={bad}
-      total={total}
-      positive={positive}
-      />
-      
+      {anecdotes[selected]}
+      <button onClick={randomAnecdote}>Random anecdote</button>
     </div>
   )
 }
+
 export default App
