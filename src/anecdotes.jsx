8ee743118b45917a92b3726ac9f39320254a338c
diegoram23 <diego.pta.oc@gmail.com>
2023-02-25 17:09:55 -0800

    completed exercise 1.13

---
+++ src/anecdotes.jsx
@@ -0,0 +1,38 @@
+export default
+    [
+        {
+            id: 1,
+            value: 'If it hurts, do it more often.',
+            count: 0
+        },
+
+        {
+            id: 2,
+            value: 'Adding manpower to a late software project makes it later!',
+            count: 0
+        },
+
+        {
+            id: 3,
+            value: 'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+            count: 0
+        },
+
+        {
+            id: 4,
+            value: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+            count: 0
+        },
+
+        {
+            id: 4,
+            value: 'Premature optimization is the root of all evil.',
+            count: 0
+        },
+
+        {
+            id: 5,
+            value: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+            count: 0
+        }
+    ]
